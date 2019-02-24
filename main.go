13720558de15ef58171e4991fd09908c090c903e
Sam Carter <samcarter@Sams-Mac-mini.broadband>
2019-02-24 10:01:06 +0000

    list certs endpoints

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
 	"io"
@@ -15,60 +16,88 @@ var CertificateDB []certificate
 func Certificate(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if r.Method == "POST" {
-		//TODO check cert with id exists?
-
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			fmt.Println("error: ", err)
 		}
-
-		fmt.Println(string(body))
-
-		var newCert certificate
-		err = json.Unmarshal(body, &newCert)
-		if err != nil {
-			fmt.Println("error: ", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if newCert.ID == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		fmt.Println(CertificateDB)
-		CertificateDB = append(CertificateDB, newCert)
-
-		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "Cert Posted")
+		owner := r.Header.Get("Authorization")
+		PostCert(w, body, owner)
 	}
 
 	if r.Method == "DELETE" {
-		for i, cert := range CertificateDB {
-
-			if cert.ID == vars["id"] {
-				CertificateDB = append(CertificateDB[:i], CertificateDB[i+1:]...)
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		DeleteCert(vars["id"], w)
 	}
 
 	if r.Method == "PATCH" {
-
+		err := DeleteCert(vars["id"], w)
+		if err != nil {
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("error: ", err)
+		}
+		PostCert(w, body, "")
 	}
 }
 
-func UserCertificates(w http.ResponseWriter, r *http.Request) {
+func PostCert(w http.ResponseWriter, body []byte, owner string) error {
+	//TODO check cert with id exists?
+
+	fmt.Println(string(body))
+	fmt.Println(owner) //TODO write owner to cert
+
+	var newCert certificate
+	err := json.Unmarshal(body, &newCert)
+	if err != nil {
+		fmt.Println("error: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return errors.New("bad request")
+	}
+
+	if newCert.ID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return errors.New("bad request")
+	}
+
+	fmt.Println(CertificateDB)
+	CertificateDB = append(CertificateDB, newCert)
+
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "RetrievingUserCertificates")
-	//out, err := json.Marshal(testcert)
-	//if err != nil {
-	//	fmt.Println("error:", err)
-	//}
+	io.WriteString(w, "Cert Posted")
+	return nil
+}
+
+func DeleteCert(id string, w http.ResponseWriter) error {
+	for i, cert := range CertificateDB {
+
+		if cert.ID == id {
+			CertificateDB = append(CertificateDB[:i], CertificateDB[i+1:]...)
+			w.WriteHeader(http.StatusOK)
+			return nil
+		}
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	return errors.New("bad request")
+
+}
+
+func UserCertificates(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var matchingUserCertificates []certificate
+	for _, cert := range CertificateDB {
+		if cert.OwnerID == vars["userId"] {
+			matchingUserCertificates = append(matchingUserCertificates, cert)
+		}
+	}
+	out, err := json.Marshal(matchingUserCertificates)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(out))
+	return
 }
 
 func CertificateTransfer(w http.ResponseWriter, r *http.Request) {
@@ -76,10 +105,16 @@ func CertificateTransfer(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "CertificateTransferHandler")
 }
 
+func Reset(w http.ResponseWriter, r *http.Request) {
+	CertificateDB = nil
+	return
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/certificates/{id:[0-9a-zA-Z]+}", Certificate).Methods("POST", "DELETE", "PATCH")
 	router.HandleFunc("/certificates/{id:[0-9a-zA-Z]+}/transfer", CertificateTransfer).Methods("PATCH")
 	router.HandleFunc("/users/{userId:[0-9a-zA-Z]+}/certificates", UserCertificates).Methods("GET")
+	router.HandleFunc("/reset", Reset) //Debug helper method in lieu of test database
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 )
 
+// TODO test for owner header set
 func TestCertificateHandlerPost(t *testing.T) {
 
 	testData := `{"ID": "1", "Title": "i am a title", "CreatedAt": 0, "OwnerID": "", "Year": 1990, "Note": ""}`
@@ -71,7 +72,7 @@ func TestDeleteCertificateDoesntExist(t *testing.T) {
 }
 
 func TestDeleteCertificateDoesExist(t *testing.T) {
-	loadElement()
+	loadElement("sam")
 	req, err := http.NewRequest("DELETE", "/certificates/1", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -88,10 +89,68 @@ func TestDeleteCertificateDoesExist(t *testing.T) {
 	}
 }
 
-func loadElement() {
-	testData := `{"ID": "1", "Title": "i am a title", "CreatedAt": 0, "OwnerID": "", "Year": 1990, "Note": ""}`
+func TestPatchCertificateDoesExist(t *testing.T) {
+	loadElement("sam")
+	testData := `{"ID": "2", "Title": "i am a patched title", "CreatedAt": 4, "OwnerID": "", "Year": 1890, "Note": "new note"}`
+	req, err := http.NewRequest("PATCH", "/certificates/1", bytes.NewBuffer([]byte(testData)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req = mux.SetURLVars(req, map[string]string{"id": "1"})
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Certificate)
+
+	handler.ServeHTTP(rr, req)
+}
+
+func TestUserNoCerts(t *testing.T) {
+	reset()
+	req, err := http.NewRequest("GET", "/users/sam/certificates", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UserCertificates)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	if rr.Body.String() != "null" {
+		t.Errorf("body not correct: got %x expected %v", rr.Body.String(), "null")
+	}
+}
+
+func TestUserWithCerts(t *testing.T) {
+	loadElement("sam")
+	req, err := http.NewRequest("GET", "/users/sam/certificates", nil)
+	req = mux.SetURLVars(req, map[string]string{"userId": "sam"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UserCertificates)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	if rr.Body.String() == "null" {
+		t.Errorf("body not correct: got %x expected some data", rr.Body.String())
+	}
+}
+
+func loadElement(userID string) {
+	testData := `{"ID": "1", "Title": "i am a title", "CreatedAt": 0, "OwnerID": "` + userID + `", "Year": 1990, "Note": ""}`
 
 	req, err := http.NewRequest("POST", "/certificates/1", bytes.NewBuffer([]byte(testData)))
+	req = mux.SetURLVars(req, map[string]string{"userId": "sam"})
 	if err != nil {
 		errors.New("load element error")
 	}
@@ -101,4 +160,11 @@ func loadElement() {
 
 	handler.ServeHTTP(rr, req)
 
+}
+
+func reset() {
+	req, _ := http.NewRequest("GET", "/reset", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Reset)
+	handler.ServeHTTP(rr, req)
 }
